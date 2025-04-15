568093c39bc95248fde7efcb887eca8976ec6a0a
Irbis_neto <irbis@netology.com>
2025-04-15 22:14:21 +0300

    corrected hw_05_08_3_rework

--- hw_5.8/hw_05_08_task_3/hw_05_08_3_rework.cpp
+++ hw_5.8/hw_05_08_task_3/hw_05_08_3_rework.cpp
@@ -1,57 +1,71 @@
 // Задача 3*. std::unique_ptr своими руками
 
-
 #include <iostream>
 #include <vector>
 #include <memory>
 #include <exception>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 
-template<typename T>
+template <typename T>
 class MyUniquePtr
 {
-    public:
-        MyUniquePtr(T* ptr) : u_ptr{ptr} { }
-        ~MyUniquePtr() { 
-            if(u_ptr){
-                std::cout << "Free memory...";
-                delete u_ptr;
-            } 
+public:
+    MyUniquePtr(T *ptr) : u_ptr{ptr} {}
+    ~MyUniquePtr()
+    {
+        if (u_ptr)
+        {
+            std::cout << "Free memory...";
+            delete u_ptr;
         }
+    }
 
-        MyUniquePtr(const MyUniquePtr&) = delete;                  // удаляем конструктор копирования из класса 
-        
-        MyUniquePtr& operator=(const MyUniquePtr&) = delete;       // удаляем оператор присваивания
+    MyUniquePtr(const MyUniquePtr &) = delete; // удаляем конструктор копирования из класса
 
-        T& operator*() { 
-            return *get_val(); 
+    MyUniquePtr &operator=(const MyUniquePtr &) = delete; // удаляем оператор присваивания
+
+    T &operator*()
+    {
+        if (u_ptr == nullptr)
+        {
+            throw std::invalid_argument("Ошибка! nullptr ");
         }
-
-        T* operator->() { 
-            return get_val(); 
+        else
+        {
+            return *u_ptr;
         }
+    }
 
-        T* release() {
-            T* new_ptr = u_ptr;
-            u_ptr = nullptr;
-            return new_ptr;
+    T *operator->()
+    {
+        return get_val();
+    }
+
+    T *release()
+    {
+        T *new_ptr = u_ptr;
+        u_ptr = nullptr;
+        return new_ptr;
+    }
+
+    T *get_val()
+    {
+        if (!u_ptr)
+        {
+            throw std::invalid_argument("Ошибка! nullptr ");
         }
-
-        T* get_val() { 
-            if(!u_ptr){
-                throw std::invalid_argument("Ошибка! nullptr "); 
-            } else {
-                return u_ptr; 
-            }
+        else
+        {
+            return u_ptr;
         }
+    }
 
-    private:
-        T* u_ptr; 
+private:
+    T *u_ptr;
 };
-
 
 int main()
 {
