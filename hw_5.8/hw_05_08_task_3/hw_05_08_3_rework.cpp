a141a81b5025171e33eeb6c01f69c7c619b83434
user <user@astra>
2025-04-15 10:12:33 +0300

    added rework file for task 5.8.3*

---
+++ hw_5.8/hw_05_08_task_3/hw_05_08_3_rework.cpp
@@ -0,0 +1,59 @@
+// Задача 3*. std::unique_ptr своими руками
+
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <exception>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+template<typename T>
+class MyUniquePtr
+{
+    public:
+        MyUniquePtr(T* ptr) : u_ptr{ptr} { }
+        ~MyUniquePtr() { 
+            if(u_ptr){
+                std::cout << "Free memory...";
+                delete u_ptr;
+            } 
+        }
+
+        MyUniquePtr(const MyUniquePtr&) = delete;                  // удаляем конструктор копирования из класса 
+        
+        MyUniquePtr& operator=(const MyUniquePtr&) = delete;       // удаляем оператор присваивания
+
+        T& operator*() { 
+            return *get_val(); 
+        }
+
+        T* operator->() { 
+            return get_val(); 
+        }
+
+        T* release() {
+            T* new_ptr = u_ptr;
+            u_ptr = nullptr;
+            return new_ptr;
+        }
+
+        T* get_val() { 
+            if(!u_ptr){
+                throw std::invalid_argument("Ошибка! nullptr "); 
+            } else {
+                return u_ptr; 
+            }
+        }
+
+    private:
+        T* u_ptr; 
+};
+
+
+int main()
+{
+    return 0;
+}
